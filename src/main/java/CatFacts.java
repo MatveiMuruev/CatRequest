2710789dc62b3ba6b6670deae8a982306e17c9a6
MatveiMuruev <Matvei_Muruev@mail.ru>
2022-09-05 10:04:02 +0800

    initial commit

---
+++ src/main/java/CatFacts.java
@@ -0,0 +1,47 @@
+public class CatFacts {
+    String id;
+    String text;
+    String type;
+    String user;
+    int upvotes;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public int getUpvotes() {
+        return upvotes;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setUpvotes(int upvotes) {
+        this.upvotes = upvotes;
+    }
+}
---
+++ src/main/java/CatRequest.java
@@ -0,0 +1,38 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CatRequest {
+    private final static String URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        HttpGet get = new HttpGet(URI);
+        CloseableHttpResponse response = httpClient.execute(get);
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<CatFacts> catFactsList = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {
+        });
+
+        Stream stream = catFactsList.stream().filter(value -> value.getUpvotes() > 0)
+                .map(CatFacts::getText);
+        stream
+                .forEach(System.out::println);
+    }
+}
